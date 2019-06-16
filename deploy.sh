defc5c2426f1f02d96727a9f4c1b87f2eca12f71
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2019-06-16 16:13:05 +0700

    Add DBMS

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,13 @@ ${KUBERNETES_CLIENT} apply -f volume/
 echo 'deploying config maps ...'
 ${KUBERNETES_CLIENT} create configmap sp-manager-bin --from-file=configs/manager/bin/
 ${KUBERNETES_CLIENT} create configmap sp-manager-conf --from-file=configs/manager/conf/
+${KUBERNETES_CLIENT} create configmap sp-worker-bin --from-file=configs/worker/bin/
+${KUBERNETES_CLIENT} create configmap sp-worker-conf --from-file=configs/worker/conf/
+${KUBERNETES_CLIENT} create configmap mysql-dbscripts --from-file=configs/dbms/
 
 echo 'deploying Stream Processor manager profile and services...'
+${KUBERNETES_CLIENT} apply -f dbms.yaml
 ${KUBERNETES_CLIENT} apply -f manager.yaml
-
-
-
-
+${KUBERNETES_CLIENT} apply -f worker.yaml
+${KUBERNETES_CLIENT} apply -f receiver.yaml
+${KUBERNETES_CLIENT} apply -f dashboard.yaml
