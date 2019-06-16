4a69b2ecdd24785d3a0055bf7297688cf08e395a
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2019-06-16 14:53:29 +0700

    add PersistentVolumeClaim

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,16 @@
 
 KUBERNETES_CLIENT=`which kubectl`
 
+echo 'create volume'
+${KUBERNETES_CLIENT} apply -f volume/
 
 echo 'deploying config maps ...'
-${KUBERNETES_CLIENT} create configmap sp-manager-bin --from-file=confs/manager/bin/
-${KUBERNETES_CLIENT} create configmap sp-manager-conf --from-file=confs/manager/conf/
+${KUBERNETES_CLIENT} create configmap sp-manager-bin --from-file=configs/manager/bin/
+${KUBERNETES_CLIENT} create configmap sp-manager-conf --from-file=configs/manager/conf/
 
 echo 'deploying Stream Processor manager profile and services...'
 ${KUBERNETES_CLIENT} apply -f manager.yaml
+
+
+
+
